bdd40e02c0a3c13cd620b2f6c2269882592ca4f7
RyanTLG <ryankotze193@gmail.com>
2022-07-23 14:03:32 +0200

    damn

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
-const electron = require('electron');
-const proc = require('child_process');
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
 
-console.log(electron);
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
 
-const child = proc.spawn(electron);
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
+    })
+})
+
+// app.whenReady().then(() => {
+//   createWindow()
+
+//   app.on('activate', () => {
+//     if (BrowserWindow.getAllWindows().length === 0) {
+//       createWindow()
+//     }
+//   })
+// })
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
